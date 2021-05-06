ff0c82388286bfd6e63ecf80fa160d2cdb299de2
jpoley <jason.poley@gmail.com>
2021-05-05 23:47:04 -0400

    sg filter

--- 01-vpc/export.sh
+++ 01-vpc/export.sh
@@ -7,6 +7,5 @@ export AZ_A=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vp
 export AZ_B=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vpc-Public-B| jq -r '.Subnets[].AvailabilityZone'`
 export AZ_C=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vpc-Public-C| jq -r '.Subnets[].AvailabilityZone'`
 export AZ_D=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vpc-Public-D| jq -r '.Subnets[].AvailabilityZone'`
-
-
+export SG=`aws ec2 describe-security-groups --filters Name=tag:aws:cloudformation:stack-name,Values=aws-pug-vpc | jq -r '.SecurityGroups[].GroupId'`
 
