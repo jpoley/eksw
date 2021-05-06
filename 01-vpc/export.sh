2673ea68213092eb39ef3ec388ba59be8560c3c6
jpoley <jason.poley@gmail.com>
2021-05-05 21:50:43 -0400

    az fixes

--- 01-vpc/export.sh
+++ 01-vpc/export.sh
@@ -3,5 +3,10 @@ export SUBNET_A=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pu
 export SUBNET_B=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vpc-Public-B| jq -r '.Subnets[].SubnetId'`
 export SUBNET_C=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vpc-Public-C| jq -r '.Subnets[].SubnetId'`
 export SUBNET_D=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vpc-Public-D| jq -r '.Subnets[].SubnetId'`
+export AZ_A=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vpc-Public-A| jq -r '.Subnets[].AvailabilityZone'`
+export AZ_B=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vpc-Public-B| jq -r '.Subnets[].AvailabilityZone'`
+export AZ_C=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vpc-Public-C| jq -r '.Subnets[].AvailabilityZone'`
+export AZ_D=`aws ec2 describe-subnets  --filters Name=tag:Name,Values=aws-pug-vpc-Public-D| jq -r '.Subnets[].AvailabilityZone'`
+
 export MY_IP=`curl http://ipinfo.io/ip`
 
--- 01-vpc/teardown.sh
+++ 01-vpc/teardown.sh
@@ -1,4 +1,3 @@
 
-export C9_ID=`aws cloud9 list-environments | jq -r '.environmentIds[0]'`
-aws cloud9 delete-environment --environment-id $C9_ID
-#aws cloudformation delete-stack  --stack-name=aws-pug-vpc 
+
+aws cloudformation delete-stack  --stack-name=aws-pug-vpc 
