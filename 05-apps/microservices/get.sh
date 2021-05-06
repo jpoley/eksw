dc3598998cfa5fae15f99764ed2e818b3c020952
jpoley <jason.poley@gmail.com>
2021-05-06 13:20:27 -0400

    more demo

---
+++ 05-apps/microservices/get.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/brentley/ecsdemo-frontend.git
+git clone https://github.com/brentley/ecsdemo-nodejs.git
+git clone https://github.com/brentley/ecsdemo-crystal.git
---
+++ 05-apps/nginx-helm/helm-1.sh
@@ -0,0 +1,7 @@
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+
